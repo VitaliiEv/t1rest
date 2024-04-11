b4069064db88d8fe49c03d8c7373961c3b084280
Vitalii Solomonov <solomonov.v@gmail.com>
2024-04-11 08:02:35 +0300

    feat: init empty project

---
+++ src/test/java/com/github/vitaliiev/t1rest/T1restApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vitaliiev.t1rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1restApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
